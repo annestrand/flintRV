ab434de263bcb71a0957fa026da3219bc702307c
Austin Annestrand <austin.annestrand@gmail.com>
2022-04-16 18:33:47 -0500

    Fix immgen widths

--- fetch_decode.v
+++ fetch_decode.v
@@ -11,10 +11,10 @@ module ImmGen
         case (`OPCODE(instr))
             default          :   imm = 32'd0;
         // Immediate cases
-            `I_JUMP, `I_LOAD :   imm = {{22{instr[31]}}, instr[30:20]};
-            `I_ARITH         :   imm = isShiftImm ? {{27{instr[31]}}, instr[24:20]} : {{22{instr[31]}}, instr[30:20]};
-            `S               :   imm = {{22{instr[31]}}, instr[30:25], instr[11:8], instr[7]};
-            `B               :   imm = {{21{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'd0};
+            `I_JUMP, `I_LOAD :   imm = {{21{instr[31]}}, instr[30:20]};
+            `I_ARITH         :   imm = isShiftImm ? {{27{instr[31]}}, instr[24:20]} : {{21{instr[31]}}, instr[30:20]};
+            `S               :   imm = {{21{instr[31]}}, instr[30:25], instr[11:8], instr[7]};
+            `B               :   imm = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'd0};
             `U_LUI, `U_AUIPC :   imm = {instr[31], instr[30:20], instr[19:12], 12'd0};
             `J               :   imm = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:25], instr[24:21], 1'd0};
         endcase
