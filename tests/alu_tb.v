79f63c1e7fdfdf773a55cc51dc64a0404077121f
Austin Annestrand <austin.annestrand@gmail.com>
2022-04-17 21:33:24 -0500

    Print out zflag in unit test

--- tests/alu_tb.v
+++ tests/alu_tb.v
@@ -47,8 +47,8 @@ defparam Alu_dut.ALU_OP_COUNT = 16;
             if ($signed(result) != $signed(test_gold_vector[i][32:1]))  resultStr = "ERROR";
             else if (zflag != test_gold_vector[i][0])                   resultStr = "ERROR";
             else                                                        resultStr = "PASS ";
-            $display("Test[ %2d ]: a = %10d | b = %10d | op = %2d || result = %10d ... %s",
-                i, $signed(a), $signed(b), op, $signed(result), resultStr
+            $display("Test[ %2d ]: a = %10d | b = %10d | op = %2d || result = %10d | zflag = %d ... %s",
+                i, $signed(a), $signed(b), op, $signed(result), zflag, resultStr
             );
             if (resultStr == "ERROR") errs = errs + 1;
         end
