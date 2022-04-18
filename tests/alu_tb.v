d53deeca2c2c22119c0f5665a5a1e842484e83af
Austin Annestrand <austin.annestrand@gmail.com>
2022-04-17 21:42:41 -0500

    Cleanup alu unit test error checking

--- tests/alu_tb.v
+++ tests/alu_tb.v
@@ -44,9 +44,8 @@ defparam Alu_dut.ALU_OP_COUNT = 16;
             subfail = 0;
             {a,b,op} = test_vector[i];
             #20;
-            if ($signed(result) != $signed(test_gold_vector[i][32:1]))  resultStr = "ERROR";
-            else if (zflag != test_gold_vector[i][0])                   resultStr = "ERROR";
-            else                                                        resultStr = "PASS ";
+            if ({$signed(result), zflag} != $signed(test_gold_vector[i]))   resultStr = "ERROR";
+            else                                                            resultStr = "PASS ";
             $display("Test[ %2d ]: a = %10d | b = %10d | op = %2d || result = %10d | zflag = %d ... %s",
                 i, $signed(a), $signed(b), op, $signed(result), zflag, resultStr
             );
