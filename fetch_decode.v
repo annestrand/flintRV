e65eb87aba969815b03f5d8e3d27c176d3de22e3
Austin Annestrand <austin.annestrand@gmail.com>
2022-04-27 19:28:27 -0500

    Add i/o port name logic for fetch/decoder

--- fetch_decode.v
+++ fetch_decode.v
@@ -2,21 +2,23 @@
 
 module ImmGen
 (
-    input       [31:0]  instr,
-    output reg  [31:0]  imm
+    input       [31:0]  i_instr,
+    output reg  [31:0]  o_imm
 );
-    wire [2:0] funct3   = `FUNCT3(instr);
+    wire [2:0] funct3   = `FUNCT3(i_instr);
     wire isShiftImm     = ~funct3[1] && funct3[0];
     always @* begin
-        case (`OPCODE(instr))
-            default          :   imm = 32'd0;
+        case (`OPCODE(i_instr))
+            default  :  o_imm = 32'd0;
         // Immediate cases
-            `I_JUMP, `I_LOAD :   imm = {{21{instr[31]}}, instr[30:20]};
-            `I_ARITH         :   imm = isShiftImm ? {{27{instr[31]}}, instr[24:20]} : {{21{instr[31]}}, instr[30:20]};
-            `S               :   imm = {{21{instr[31]}}, instr[30:25], instr[11:8], instr[7]};
-            `B               :   imm = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'd0};
-            `U_LUI, `U_AUIPC :   imm = {instr[31], instr[30:20], instr[19:12], 12'd0};
-            `J               :   imm = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:25], instr[24:21], 1'd0};
+            `I_JUMP  ,
+            `I_LOAD  : o_imm = {{21{i_instr[31]}}, i_instr[30:20]};
+            `I_ARITH : o_imm = isShiftImm ? {{27{i_instr[31]}}, i_instr[24:20]} : {{21{i_instr[31]}}, i_instr[30:20]};
+            `S       : o_imm = {{21{i_instr[31]}}, i_instr[30:25], i_instr[11:8], i_instr[7]};
+            `B       : o_imm = {{20{i_instr[31]}}, i_instr[7], i_instr[30:25], i_instr[11:8], 1'd0};
+            `U_LUI   ,
+            `U_AUIPC : o_imm = {i_instr[31], i_instr[30:20], i_instr[19:12], 12'd0};
+            `J       : o_imm = {{12{i_instr[31]}}, i_instr[19:12], i_instr[20], i_instr[30:25], i_instr[24:21], 1'd0};
         endcase
     end
 endmodule
@@ -28,8 +30,8 @@ endmodule
 `undef INSTR
 module Controller
 (
-    input       [31:0]  instr,
-    output  reg [15:0]  ctrlSignals
+    input       [31:0]  i_instr,
+    output  reg [15:0]  o_ctrlSignals
 );
     parameter           INSTR_COUNT         = 40; // Default RV32I count
     localparam          INSTR_ADDR_WIDTH    = $clog2(INSTR_COUNT);
@@ -41,13 +43,13 @@ module Controller
     reg     [1:0]                   instrOpEnc;
     reg     [16:0]                  instrOpEncAddr;
     reg     [INSTR_ADDR_WIDTH-1:0]  instrAddr;
-    wire    [16:0]                  F7_F3_OP_wire   = {`FUNCT7(instr), `FUNCT3(instr), `OPCODE(instr)};
-    wire    [16:0]                  F3_OP_wire      = {7'd0          , `FUNCT3(instr), `OPCODE(instr)};
-    wire    [16:0]                  OP_wire         = {7'd0          , 3'd0          , `OPCODE(instr)};
-    wire    [2:0]                   funct3          = `FUNCT3(instr);
+    wire    [16:0]                  F7_F3_OP_wire   = {`FUNCT7(i_instr), `FUNCT3(i_instr), `OPCODE(i_instr)};
+    wire    [16:0]                  F3_OP_wire      = {7'd0          , `FUNCT3(i_instr), `OPCODE(i_instr)};
+    wire    [16:0]                  OP_wire         = {7'd0          , 3'd0          , `OPCODE(i_instr)};
+    wire    [2:0]                   funct3          = `FUNCT3(i_instr);
     wire                            isShiftImm      = ~funct3[1] && funct3[0];
     always @* begin
-        case (`OPCODE(instr))   // 1. Get instruction type
+        case (`OPCODE(i_instr))   // 1. Get instruction type
             default                                      : instrOpEnc = F7_F3_OP;
             `R                                           : instrOpEnc = F7_F3_OP;
             `I_JUMP, `I_LOAD, `I_FENCE, `I_SYS           : instrOpEnc = F3_OP;
@@ -74,9 +76,9 @@ module Controller
     // instr output assignment
     always @* begin
         case (instrAddr)
-            default : ctrlSignals = 'd0;
+            default : o_ctrlSignals = 'd0;
             `define INSTR(NAME, ISA_ENC, ENC, EX_OP, EXEA, EXEB, LDEXT, MEMR, MEMW, REGW, M2R, BRA, JMP) \
-                ENC : ctrlSignals = {EX_OP, EXEA, EXEB, LDEXT, MEMR, MEMW, REGW, M2R, BRA, JMP};
+                ENC : o_ctrlSignals = {EX_OP, EXEA, EXEB, LDEXT, MEMR, MEMW, REGW, M2R, BRA, JMP};
             `include "instr.vh"
             `undef INSTR
         endcase
@@ -87,12 +89,18 @@ endmodule
 // Main fetch-decode stage wrapper module
 module FetchDecode
 (
-    input   [31:0] instr,
-    output  [31:0] imm,
-    output  [15:0] ctrlSignals
+    input   [31:0] i_instr,
+    output  [31:0] o_imm,
+    output  [15:0] o_ctrlSignals
 );
 
-    ImmGen      IMMGEN_unit(.instr(instr), .imm(imm));
-    Controller  CTRL_unit(.instr(instr), .ctrlSignals(ctrlSignals));
+    ImmGen IMMGEN_unit(
+        .i_instr(i_instr),
+        .o_imm(o_imm)
+    );
+    Controller CTRL_unit(
+        .i_instr(i_instr),
+        .o_ctrlSignals(o_ctrlSignals)
+    );
 
 endmodule
--- tests/immgen_tb.v
+++ tests/immgen_tb.v
@@ -2,8 +2,8 @@
 `include "fetch_decode.v"
 
 module ImmGen_tb;
-    reg     [31:0]  instr;
-    wire    [31:0]  imm;
+    reg     [31:0]  i_instr;
+    wire    [31:0]  o_imm;
 
     ImmGen ImmGen_dut(.*);
 
@@ -27,16 +27,16 @@ module ImmGen_tb;
     integer i = 0, errs = 0;
     initial begin
         $display("Running random ImmGen tests...\n");
-        instr   = 'd0;
+        i_instr   = 'd0;
         #20;
         for (i=0; i<27; i=i+1) begin
             // Note: RISC-V Verilog Objcopy seems to output big-endian for some reason, swap to little here
-            instr = `ENDIAN_SWP_32(test_vector[i]);
+            i_instr = `ENDIAN_SWP_32(test_vector[i]);
             #20;
-            if ($signed(imm) != $signed(test_gold_vector[i]))   resultStr = "ERROR";
+            if ($signed(o_imm) != $signed(test_gold_vector[i])) resultStr = "ERROR";
             else                                                resultStr = "PASS ";
-            $display("Test[ %2d ]: instr = 0x%8h || imm = %b ... %s",
-                i, instr, $signed(imm), resultStr
+            $display("Test[ %2d ]: i_instr = 0x%8h || o_imm = %b ... %s",
+                i, i_instr, $signed(o_imm), resultStr
             );
             if (resultStr == "ERROR") errs = errs + 1;
         end
