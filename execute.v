b48e71f442c6471736b4763917c155658a09837f
Austin Annestrand <austin.annestrand@gmail.com>
2022-05-02 21:01:15 -0500

    Added "Execute" wrapper module

--- execute.v
+++ execute.v
@@ -194,3 +194,66 @@ module Alu
     // Zero-flag out
     assign zflag = ~|result;
 endmodule
+
+// ====================================================================================================================
+// Main execute stage wrapper module
+module Execute
+(
+    input   [6:0]   funct7,
+    input   [2:0]   funct3,
+    input   [3:0]   aluOp,
+    input   [1:0]   fwdRs1, fwdRs2,
+    input           aluSrcA, aluSrcB
+    input   [31:0]  EXEC_rs1, MEM_rs1, WB_rs1, EXEC_rs2, MEM_rs2, WB_rs2,
+    input   [31:0]  PC, IMM,
+    output  [31:0]  aluOut, addrGenOut
+);
+
+    // Datapath for register forwarding
+    reg [31:0] rs1Out, rs2Out;
+    always@(*) begin
+        case (fwdRs1)
+            `NO_FWD     : rs1Out = EXEC_rs1;
+            `FWD_MEM    : rs1Out = MEM_rs1;
+            `FWD_WB     : rs1Out = WB_rs1;
+            default     : rs1Out = EXEC_rs1;
+        endcase
+        case (fwdRs2)
+            `NO_FWD     : rs2Out = EXEC_rs2;
+            `FWD_MEM    : rs2Out = MEM_rs2;
+            `FWD_WB     : rs2Out = WB_rs2;
+            default     : rs2Out = EXEC_rs2;
+        endcase
+    end
+
+    // Datapath for ALU srcs
+    wire [31:0] aluSrcAin = (aluSrcA == `PC ) ? PC  : rs1Out;
+    wire [31:0] aluSrcBin = (aluSrcA == `IMM) ? IMM : rs2Out;
+
+    // ALU/ALU_Control
+    wire [4:0]  aluControl;
+    AluControl ALU_CTRL_unit(
+        .aluOp      (aluOp),
+        .funct7     (funct7),
+        .funct3     (funct3),
+        .aluControl (aluControl)
+    );
+    Alu ALU_unit(
+        .a      (aluSrcAin),
+        .b      (aluSrcBin),
+        .op     (aluControl),
+        .result (aluOut),
+        .zflag  (/* No use for now... */)
+    );
+    defparam Alu_dut.WIDTH = 32;
+    defparam Alu_dut.ALU_OP_COUNT = 16;
+
+    // Address generator
+    CLA ADDR_GEN_unit(
+        .a      (PC),
+        .b      (IMM),
+        .subEn  (0),
+        .result (addrGenOut),
+        .cout   (/* No use for now... */)
+    );
+endmodule
