e705fafbdba23de00257644f10e3f91e3a71ff0d
Austin Annestrand <austin.annestrand@gmail.com>
2022-05-02 21:21:42 -0500

    Added "Writeback" module

---
+++ writeback.v
@@ -0,0 +1,18 @@
+// ====================================================================================================================
+module Writeback
+(
+    input       [2:0]   funct3,
+    input       [31:0]  dataIn,
+    output reg  [31:0]  dataOut
+);
+    // Just output load-type (w/ - w/o sign-ext) for now
+    always @(*) begin
+        case (funct3)
+            `LS_B_OP    : dataOut = {{24{dataIn[31]}}, dataIn[7:0]};
+            `LS_H_OP    : dataOut = {{16{dataIn[31]}}, dataIn[15:0]};
+            `LS_W_OP    : dataOut = dataIn;
+            `LS_BU_OP   : dataOut = {24'd0, dataIn[7:0]};
+            `LS_HU_OP   : dataOut = {16'd0, dataIn[15:0]};
+            default     : dataOut = dataIn;
+    end
+endmodule
