5eb3093a8123a11519800285eb4aed43adabbafe
Austin Annestrand <austin.annestrand@gmail.com>
2022-09-04 02:35:09 -0500

    Added binary search algorithm test

--- tests/cpu/algorithms/test_algorithms.cc
+++ tests/cpu/algorithms/test_algorithms.cc
@@ -42,4 +42,32 @@ TEST(algorithms, fibonacci) {
     EXPECT_EQ(dut.readRegfile(S8), fibonacci(8));
     EXPECT_EQ(dut.readRegfile(S9), fibonacci(9));
     EXPECT_EQ(dut.readRegfile(S10), fibonacci(10));
+    EXPECT_EQ(dut.readRegfile(doneReg), -1); // Make sure we did not time-out in simulation
+}
+// ====================================================================================================================
+TEST(algorithms, binsearch) {
+    boredcore dut = boredcore(100000);
+    if (!dut.create(new Vboredcore(), "obj_dir/binsearch.vcd")) { FAIL(); }
+    if (!dut.createMemory(0x2000, BASE_PATH "/binsearch.mem"))  { FAIL(); }
+
+    bool done                   = false;
+    constexpr int doneReg       = S11;
+    constexpr int simDoneVal    = -1;
+    dut.m_cpu->i_ifValid        = 1;    // Always valid since we assume combinatorial read/write for test memory
+    dut.m_cpu->i_memValid       = 1;    // Always valid since we assume combinatorial read/write for test memory
+
+    while (!dut.end() && !done) {
+        if (!dut.instructionUpdate())    { FAIL(); }
+        if (!dut.loadMemUpdate())        { FAIL(); }
+        if (!dut.storeMemUpdate())       { FAIL(); }
+        done = dut.readRegfile(doneReg) == simDoneVal;
+        dut.tick(); // Evaluate
+    }
+
+    // Check results
+    EXPECT_EQ(dut.readRegfile(S1),   1);        // Testing valid binsearch result
+    EXPECT_EQ(dut.readRegfile(S2),   1);        // Testing valid binsearch result
+    EXPECT_EQ(dut.readRegfile(S3),   1);        // Testing valid binsearch result
+    EXPECT_EQ(dut.readRegfile(S4),   0);        // Testing invalid binsearch result
+    EXPECT_EQ(dut.readRegfile(doneReg), -1);    // Make sure we did not time-out in simulation
 }
