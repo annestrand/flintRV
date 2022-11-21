e07f73387ab0e7d0831a8ebc139d857d27a02b4e
Austin Annestrand <austin.annestrand@gmail.com>
2022-11-20 21:03:12 -0600

    Refactor Control Unit test

--- tests/sub/ControlUnit_tb.v
+++ tests/sub/ControlUnit_tb.v
@@ -1,3 +1,5 @@
+`include "types.vh"
+
 module ControlUnit_tb;
     reg     [6:0]   i_opcode;
     wire    [3:0]   o_aluOp;
@@ -6,26 +8,26 @@ module ControlUnit_tb;
     ControlUnit ControlUnit_dut(.*);
 
     // Test vectors
-    reg [31:0]  test_vector         [0:39];
-    reg [12:0]  test_gold_vector    [0:39];
+    parameter TEST_COUNT = 2**7;
+    reg  [6:0] test_vector      [0:TEST_COUNT-1];
+    reg [10:0] test_gold_vector [0:TEST_COUNT-1];
     initial begin
-        $readmemh("build/tests/sub/sub_ControlUnit.mem", test_vector);
-    end
-    reg [31:0]  instr;
-    initial begin
-        for (i=0; i<40; i=i+1) begin
-            instr = test_vector[i];
-            if      (`OPCODE(instr) == `R      ) test_gold_vector[i] = {`R_CTRL         };
-            else if (`OPCODE(instr) == `I_JUMP ) test_gold_vector[i] = {`I_JUMP_CTRL    };
-            else if (`OPCODE(instr) == `I_LOAD ) test_gold_vector[i] = {`I_LOAD_CTRL    };
-            else if (`OPCODE(instr) == `I_ARITH) test_gold_vector[i] = {`I_ARITH_CTRL   };
-            else if (`OPCODE(instr) == `I_SYS  ) test_gold_vector[i] = {`I_SYS_CTRL     };
-            else if (`OPCODE(instr) == `I_FENCE) test_gold_vector[i] = {`I_FENCE_CTRL   };
-            else if (`OPCODE(instr) == `S      ) test_gold_vector[i] = {`S_CTRL         };
-            else if (`OPCODE(instr) == `B      ) test_gold_vector[i] = {`B_CTRL         };
-            else if (`OPCODE(instr) == `U_LUI  ) test_gold_vector[i] = {`U_LUI_CTRL     };
-            else if (`OPCODE(instr) == `U_AUIPC) test_gold_vector[i] = {`U_AUIPC_CTRL   };
-            else if (`OPCODE(instr) == `J      ) test_gold_vector[i] = {`J_CTRL         };
+        for (i=0; i<TEST_COUNT; i=i+1) begin
+            test_vector[i] = i;
+            case (i)
+                `R       : test_gold_vector[i] = `R_CTRL;
+                `I_JUMP  : test_gold_vector[i] = `I_JUMP_CTRL;
+                `I_LOAD  : test_gold_vector[i] = `I_LOAD_CTRL;
+                `I_ARITH : test_gold_vector[i] = `I_ARITH_CTRL;
+                `I_SYS   : test_gold_vector[i] = `I_SYS_CTRL;
+                `I_FENCE : test_gold_vector[i] = `I_FENCE_CTRL;
+                `S       : test_gold_vector[i] = `S_CTRL;
+                `B       : test_gold_vector[i] = `B_CTRL;
+                `U_LUI   : test_gold_vector[i] = `U_LUI_CTRL;
+                `U_AUIPC : test_gold_vector[i] = `U_AUIPC_CTRL;
+                `J       : test_gold_vector[i] = `J_CTRL;
+                default  : test_gold_vector[i] = 'd0;
+            endcase
         end
     end
 
@@ -34,12 +36,9 @@ module ControlUnit_tb;
     reg     [12:0]  ctrlSignals;
     integer i = 0, errs = 0;
     initial begin
-        i_opcode   = 'd0;
-        #20;
-        for (i=0; i<40; i=i+1) begin
-            instr       = test_vector[i];
-            i_opcode    = `OPCODE(instr);
-            #20;
+        i_opcode   = 'd0; #20;
+        for (i=0; i<TEST_COUNT; i=i+1) begin
+            i_opcode    = test_vector[i]; #20;
             ctrlSignals = {o_aluOp, o_exec_a, o_exec_b, o_mem_w, o_reg_w, o_mem2reg, o_bra, o_jmp};
             if (ctrlSignals != test_gold_vector[i]) resultStr = "ERROR";
             else                                    resultStr = "PASS ";
