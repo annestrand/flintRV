7e1dc44f5fdc71f4e8da4024e223fe3f27920408
Austin Annestrand <austin.annestrand@gmail.com>
2022-11-20 23:32:11 -0600

    Refine ALU_Control.v

--- rtl/ALU_Control.v
+++ rtl/ALU_Control.v
@@ -8,18 +8,9 @@ module ALU_Control (
 );
     always @* begin
         case (i_aluOp)
-            // ~~~ Undefined format ~~~
-            default             : o_aluControl = 5'b00000;
-            // ~~~ U/J-Type formats ~~~
-            `ALU_OP_J           : o_aluControl = `ALU_EXEC_ADD4A;
-            `ALU_OP_U_LUI       : o_aluControl = `ALU_EXEC_PASSB;
-            `ALU_OP_U_AUIPC     : o_aluControl = `ALU_EXEC_ADD;
-            // ~~~ I/S/B-Type formats ~~~
-            `ALU_OP_S           : o_aluControl = `ALU_EXEC_ADD;
-            `ALU_OP_I_SYS       : o_aluControl = `ALU_EXEC_ADD;
-            `ALU_OP_I_LOAD      : o_aluControl = `ALU_EXEC_ADD;
-            `ALU_OP_I_FENCE     : o_aluControl = `ALU_EXEC_ADD;
+            `ALU_OP_J           ,
             `ALU_OP_I_JUMP      : o_aluControl = `ALU_EXEC_ADD4A;
+            `ALU_OP_U_LUI       : o_aluControl = `ALU_EXEC_PASSB;
             `ALU_OP_B           : case (i_funct3)
                 3'b000          : o_aluControl = `ALU_EXEC_EQ;
                 3'b001          : o_aluControl = `ALU_EXEC_NEQ;
@@ -27,10 +18,9 @@ module ALU_Control (
                 3'b101          : o_aluControl = `ALU_EXEC_SGTE;
                 3'b110          : o_aluControl = `ALU_EXEC_SLTU;
                 3'b111          : o_aluControl = `ALU_EXEC_SGTEU;
-                default         : o_aluControl = 5'b00000;
+                default         : o_aluControl = `ALU_EXEC_ADD;
             endcase
             `ALU_OP_I_ARITH     : casez ({i_funct7, i_funct3})
-                10'b???????_000 : o_aluControl = `ALU_EXEC_ADD;
                 10'b???????_010 : o_aluControl = `ALU_EXEC_SLT;
                 10'b???????_011 : o_aluControl = `ALU_EXEC_SLTU;
                 10'b???????_100 : o_aluControl = `ALU_EXEC_XOR;
@@ -39,11 +29,9 @@ module ALU_Control (
                 10'b0000000_001 : o_aluControl = `ALU_EXEC_SLL;
                 10'b0000000_101 : o_aluControl = `ALU_EXEC_SRL;
                 10'b0100000_101 : o_aluControl = `ALU_EXEC_SRA;
-                default         : o_aluControl = 5'b00000;
+                default         : o_aluControl = `ALU_EXEC_ADD;
             endcase
-            // ~~~ R-Type format ~~~
             `ALU_OP_R           : case ({i_funct7, i_funct3})
-                10'b0000000_000 : o_aluControl = `ALU_EXEC_ADD;
                 10'b0100000_000 : o_aluControl = `ALU_EXEC_SUB;
                 10'b0000000_001 : o_aluControl = `ALU_EXEC_SLL;
                 10'b0000000_010 : o_aluControl = `ALU_EXEC_SLT;
@@ -53,8 +41,10 @@ module ALU_Control (
                 10'b0100000_101 : o_aluControl = `ALU_EXEC_SRA;
                 10'b0000000_110 : o_aluControl = `ALU_EXEC_OR;
                 10'b0000000_111 : o_aluControl = `ALU_EXEC_AND;
-                default         : o_aluControl = 5'b00000;
+                default         : o_aluControl = `ALU_EXEC_ADD;
             endcase
+            // `ALU_OP_I_LOAD, `ALU_OP_S, `ALU_OP_U_AUIPC, `ALU_OP_I_SYS, `ALU_OP_I_FENCE, or (undefined)
+            default             : o_aluControl = `ALU_EXEC_ADD;
         endcase
     end
 endmodule
