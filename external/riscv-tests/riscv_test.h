cf029b80d39fee66d2a9c77d2505a0405e98e21f
Austin Annestrand <austin.annestrand@gmail.com>
2022-12-11 17:59:35 -0600

    Added riscv-tests external code

---
+++ external/riscv-tests/riscv_test.h
@@ -0,0 +1,34 @@
+#ifndef RISCV_TEST_H
+#define RISCV_TEST_H
+
+#ifndef TEST_FUNC_NAME
+#define TEST_FUNC_NAME mytest
+#define TEST_FUNC_TXT "mytest"
+#define TEST_FUNC_RET mytest_ret
+#endif
+
+#define RVTEST_RV32U
+#define TESTNUM x28
+
+#define RVTEST_CODE_BEGIN           \
+    .text;                          \
+    .global TEST_FUNC_NAME;         \
+    .global TEST_FUNC_RET;
+
+#define RVTEST_PASS                 \
+    addi    a1,zero,'O';            \
+    addi    a2,zero,'K';            \
+    addi    a3,zero,  0;            \
+    ebreak;
+
+#define RVTEST_FAIL                 \
+    addi    a1,zero,'E';            \
+    addi    a2,zero,'R';            \
+    addi    a3,zero,'R';            \
+    ebreak;
+
+#define RVTEST_CODE_END
+#define RVTEST_DATA_BEGIN .balign 4;
+#define RVTEST_DATA_END
+
+#endif
